7ed7116a52c3916b872ea7c171d9ef2e3737f6c3
Popa Vlad <popa.vlad94@gmail.com>
2023-07-03 10:53:47 +0300

    Initial COMMIT

--- src/isValidCommitMesage.ts
+++ src/isValidCommitMesage.ts
@@ -14,7 +14,8 @@ const DEFAULT_COMMIT_TYPES = [
     "update",
     "wip",
     "Update: ",
-    "Initial: "
+    "Initial: ",
+    "initial"
 ];
 
 const isValidCommitMessage = (message, availableTypes = DEFAULT_COMMIT_TYPES): boolean => {
