31437650c53dbab9ac250fe33a9cf35d446664ab
Phantom Cracker <popa.vlad94@gmail.com>
2023-07-04 10:15:05 +0300

    Update src/isValidCommitMesage.ts
    Co-authored-by: アレクサンダー.eth <4975670+pavlovcik@users.noreply.github.com>

--- src/isValidCommitMesage.ts
+++ src/isValidCommitMesage.ts
@@ -13,7 +13,6 @@ const DEFAULT_COMMIT_TYPES = [
     "merge",
     "update",
     "wip",
-    "Update "
 ];
 
 const isValidCommitMessage = (message, availableTypes = DEFAULT_COMMIT_TYPES): boolean => {
