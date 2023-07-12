db884eaad191caa657259c56d4cba947fea1febf
Popa Vlad <popa.vlad94@gmail.com>
2023-07-12 14:50:53 +0300

    fix: test

--- src/main.mjs
+++ src/main.mjs
@@ -1,8 +1,8 @@
 const { context } = require("@actions/github");
 const core = require("@actions/core");
 
-import isValidCommitMessage from "./isValidCommitMesage";
-import extractCommits from "./extractCommits";
+import isValidCommitMessage from "./isValidCommitMesage.ts";
+import extractCommits from "./extractCommits.ts";
 
 async function run() {
     core.info(
