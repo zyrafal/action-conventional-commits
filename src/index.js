70568f0ea68499589e632596ea9823c6704f308a
Popa Vlad <popa.vlad94@gmail.com>
2023-07-20 14:08:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const {execSync} = require('child_process');
 
-execSync("yes 2>/dev/null | npm install");
+execSync("npm install --loglevel=error");
 
 const output = execSync("yes 2>/dev/null | npx tsx src/main.ts");
 console.log(output.toString());
