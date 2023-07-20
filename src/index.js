088e1f3268d472ca419a7dd81fc4765f37bd4926
Popa Vlad <popa.vlad94@gmail.com>
2023-07-20 14:02:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const {execSync} = require('child_process');
 
-execSync("yarn install");
+execSync("yes 2>/dev/null | yarn install");
 
 const output = execSync("yes 2>/dev/null | npx tsx src/main.ts");
 console.log(output.toString());
