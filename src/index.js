f61d4283d30e08f533794ccc9019bfb9c0272311
Popa Vlad <popa.vlad94@gmail.com>
2023-07-20 14:19:06 +0300

    fix: finish

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ const {execSync} = require('child_process');
 execSync("rm -rf ./dist");
 execSync("npm install --loglevel=error");
 
-
 const output = execSync("yes 2>/dev/null | npx tsx src/main.ts");
 console.log(output.toString());
