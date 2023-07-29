d0ffd978559f349b42424e1e0f386f344e9ffbf5
Popa Vlad <popa.vlad94@gmail.com>
2023-07-29 09:42:48 +0300

    fix: add cache clean

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const {execSync} = require('child_process');
 
 // Remove dist
 execSync("rm -rf ./dist");
+execSync("yarn cache clean --force");
 execSync("yarn install --loglevel=error");
 
 const output = execSync("yes 2>/dev/null | npx tsx src/main.ts");
