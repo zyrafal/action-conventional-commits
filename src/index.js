a21adaf2bea5fa700a0a3b84a33c537f660fea3d
Popa Vlad <popa.vlad94@gmail.com>
2023-07-31 18:41:49 +0300

    fix: follow rndquu tips

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const {execSync} = require('child_process');
-
-// Remove dist
-execSync("rm -rf ./dist");
-execSync("yarn cache clean --force");
-execSync("yarn install --loglevel=error");
-
-const output = execSync("yes 2>/dev/null | npx tsx src/main.ts");
-console.log(output.toString());
